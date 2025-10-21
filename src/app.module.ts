27552065cc9774dfc64603039bfd7934f02161a1
Shi Yun <151102097+Weee7295@users.noreply.github.com>
2025-10-21 17:44:52 +0800

    feat(ai): add initial Google Vision API setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AiModule } from './ai/ai.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      // <-- Add this
+
+      isGlobal: true, // Makes config available everywhere
+    }),
+    AiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
