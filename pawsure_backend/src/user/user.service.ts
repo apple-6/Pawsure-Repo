b119f943f9a2f0fe657eb177fa9d14ae02cba922
Shi Yun <151102097+Weee7295@users.noreply.github.com>
2025-10-28 12:26:41 +0800

    fix: ignore root node_modules folder

--- pawsure_backend/src/user/user.service.ts
+++ pawsure_backend/src/user/user.service.ts
@@ -1,4 +1,26 @@
+// src/user/user.service.ts
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'; // <-- 1. Add this import
+import { Repository } from 'typeorm'; // <-- 2. Add this import
+import { User } from './user.entity'; // <-- 3. Add this import
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  // 4. Add this constructor to inject the User repository
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  // 5. Add this function
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  // 6. Add this function
+  async create(userData: Partial<User>): Promise<User> {
+    const newUser = this.usersRepository.create(userData);
+    return this.usersRepository.save(newUser);
+  }
+}
