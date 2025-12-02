b885f6295b4db6f014086d143a6465b483894f7c
Mavis <mavis@graduate.utm.my>
2025-12-02 10:16:01 +0800

    Fix gitignore tracking

--- pawsure_backend/dist/health-record/health-record.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { HealthRecordService } from './health-record.service';
-import { CreateHealthRecordDto } from './dto/create-health-record.dto';
-export declare class HealthRecordController {
-    private readonly healthRecordService;
-    constructor(healthRecordService: HealthRecordService);
-    create(petId: number, createHealthRecordDto: CreateHealthRecordDto): Promise<import("./health-record.entity").HealthRecord>;
-    findAllForPet(petId: number): Promise<import("./health-record.entity").HealthRecord[]>;
-}
