51bab6b2875b3fc0446a410dcb5e79816a1921c1
MOHAMAD SAMY ARIDHAN HON <147996096+SamyAridhan@users.noreply.github.com>
2025-12-03 13:51:48 +0800

    Delete dist and nodemodules

--- pawsure_backend/dist/role/role.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { Role } from './role.entity';
-export declare class RoleService {
-    private readonly roleRepository;
-    constructor(roleRepository: Repository<Role>);
-    saveRole(roleName: string): Promise<Role>;
-    getAllRoles(): Promise<Role[]>;
-}
