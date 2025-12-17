72ae94ec662bbff6e8893e65de732f51bdcb7918
Kar Ying <yapkarying0901@gmail.com>
2025-12-17 14:18:07 +0800

    sitter setting

--- pawsure_app/lib/screens/sitter_setup/sitter_dashboard.dart
+++ pawsure_app/lib/screens/sitter_setup/sitter_dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart'; // <--- Import GetX
 import 'sitter_calendar.dart';
 import 'sitter_inbox.dart';
+import 'sitter_setting_screen.dart';
 
 class SitterDashboard extends StatelessWidget {
   const SitterDashboard({super.key});
@@ -54,6 +55,9 @@ class SitterDashboard extends StatelessWidget {
           // You can add other links here later (e.g. Settings is index 4)
         if (index == 3) { // Index 3 is Inbox
           Get.to(() => const SitterInbox());
+        }
+        if (index == 4) { 
+          Get.to(() => const SitterSettingPage());
         }
 
         },
